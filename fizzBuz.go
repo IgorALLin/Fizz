77523afc3aff6d36e9d34170330e55f3681d8279
igorallin <igorallin11@gmail.com>
2019-04-28 20:32:15 +0300

    init

---
+++ fizzBuz.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func fizzBuzz(val int) (res string) {
+	if val % 3 == 0 {
+		res = res + "Fizz"
+	}
+	if val % 5 == 0 {
+		res = res + "Buzz"
+	}
+
+	return
+}
+
+func main()  {
+	for i := 0; i <= 100; i++ {
+		if i != 0 && fizzBuzz(i) != "" {
+			fmt.Println(fizzBuzz(i))
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
